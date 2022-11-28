c2e4aa564860d07a14bf5f98348ca89943774be4
matsth <93190019+matsth@users.noreply.github.com>
2022-11-28 11:54:08 +0100

    Keras + Kamera Test

--- Keras + Kamera Test.py
+++ Keras + Kamera Test.py
@@ -7,8 +7,9 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from PIL import Image, ImageOps
 import io
+import time
 
-import numpy
+import numpy as np
 
 
 global model
@@ -23,23 +24,28 @@ def GetPlayed(img):
     global model
     global size
     
-    data = numpy.ndarray(shape=(1, 224, 224, 3), dtype=numpy.float32)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
     image = img
 
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
-    image_array = numpy.asarray(image)
+    image_array = np.asarray(image)
 
-    normalized_image_array = (image_array.astype(numpy.float32) / 127.0) - 1
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
     data[0] = normalized_image_array
 
     prediction = model.predict(data)
 
-    print(numpy.argmax(prediction))
+    if(np.argmax(prediction) == 0):
+        return 'Schere'
+    elif(np.argmax(prediction) == 1):
+        return 'Stein'
+    elif(np.argmax(prediction) == 2):
+        return 'Papier'
 
-
+pygame.init()
 pygame.camera.init()
 camlist = pygame.camera.list_cameras()
 
@@ -58,21 +64,31 @@ screen = pygame.display.set_mode( ( WIDTH, HEIGHT ) )
 
 pygame.display.set_caption("pyGame Camera View")
 
-while True :
-    for e in pygame.event.get() :
-        if e.type == pygame.QUIT :
-            sys.exit()
+font = pygame.font.SysFont(None, 48)
 
-    pil_string_image = pygame.image.tostring(imggame, "RGB", False)
-    pli_image = Image.frombytes('RGB', imggame.get_size(), pil_string_image, 'raw')
-    temp_io = io.BytesIO()
-    pli_image.save(temp_io, "JPEG")
-    GetPlayed(Image.open(temp_io))
+t_end = time.time()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    if not(time.time() < t_end):
+        pil_string_image = pygame.image.tostring(imggame, "RGB", False)
+        pli_image = Image.frombytes('RGB', imggame.get_size(), pil_string_image, 'raw')
+        temp_io = io.BytesIO()
+        pli_image.save(temp_io, "JPEG")
+
+        resultMachine = GetPlayed(Image.open(temp_io))
+        textsurface = font.render(resultMachine, True, (0, 0, 255))
+        t_end = time.time() + 1
         
     # draw frame
     screen.blit(imggame, (0,0))
-    pygame.display.flip()
+    screen.blit(textsurface, (20, 20))
+    #pygame.display.flip()
+    pygame.display.update()
     # grab next frame    
     imggame = cam.get_image()
-
+    
+pygame.quit()
